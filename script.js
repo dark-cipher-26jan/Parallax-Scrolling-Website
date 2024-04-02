ce15d4b52491ac520fe85e65ec800a7def2a0614
Anuz Yadav <158555259+dark-cipher-26jan@users.noreply.github.com>
2024-04-02 07:23:27 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let btn = document.getElementById('btn');
+let rocks = document.getElementById('rocks');
+let forest = document.getElementById('forest');
+let water = document.getElementById('water');
+let header = document.getElementById('header');
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    text.style.top = 50 + value * -.1 + '%';
+    bird2.style.top = value * -1.5 + 'px';
+    bird2.style.left = value * 2 + 'px';
+    bird1.style.top = value * -1.5 + 'px';
+    bird1.style.left = value * -5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    rocks.style.top = value * -.12 + 'px';
+    forest.style.top = value * .25 + 'px';
+    header.style.top = value * .5 + 'px';
+});
+
+   /*
+ * Copyright (c) 2024 Anuz
+ *
+ * Author: Anuz Yadav
+ * Email: darkcipher26jan2024@gmail.com
+ * GitHub: https://github.com/dark-cipher-26jan
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
